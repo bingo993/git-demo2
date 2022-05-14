0ba32336e2f90fd1d93e5b2dc860f3deaa0c82b3
bingo <bingo@atguigu.com>
2022-05-14 22:18:59 +0800

    223

--- src/main/java/com/bingo/git.java
+++ src/main/java/com/bingo/git.java
@@ -3,5 +3,7 @@ package com.bingo;
 public class git {
     static void main(String[] args) {
         System.out.println("子子");
+        System.out.println("bingo");
+        System.out.println("333");
     }
 }
